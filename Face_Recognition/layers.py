9b3c87e0c12f480d82d5bede7032b7af9a6cff3b
System_Hang <110552199+Sujal2520@users.noreply.github.com>
2023-10-06 09:40:45 +0530

    Add files via upload

---
+++ Face_Recognition/layers.py
@@ -0,0 +1,19 @@
+# (Will hold) Custom L1 Distance layer module-(its needed to load the custom model)
+
+# Import dependencies
+import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Layer
+
+
+# Custom L1 Distance Layer from Jupyter
+class L1Dist(Layer):
+    
+    # Init method- inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+        
+
+    # Similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding) 
